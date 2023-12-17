b0a40b26000be8caba248e8ad3ee0fb642175ac8
gerel <gerel@bk.ru>
2023-12-17 23:12:09 +0500

    Final

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿// Задача 1: Задайте двумерный массив символов (тип char [,]).
 //Создать строку из символов этого массива.
+using System.Reflection.Metadata;
+using System.Xml.XPath;
+
 char[,] FillArray(int rows, int columns)
 {
     char[,] result = new char[rows, columns];
@@ -27,3 +30,17 @@ void PrintArray(char[,] arr)
 }
 char[,] array = FillArray(5, 5);
 PrintArray(array);
+Console.WriteLine();
+string CharArrayToString(char[,] arr)
+{
+    string result = String.Empty;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            result += arr[i,j];
+        }
+    }
+    return result;
+}
+Console.WriteLine(CharArrayToString(array));
