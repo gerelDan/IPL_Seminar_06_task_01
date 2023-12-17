98f15aaeecfe678f0894956f6f884a9ddaaf0fc4
gerel <gerel@bk.ru>
2023-12-17 19:02:26 +0500

    Add Task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте двумерный массив символов (тип char [,]).
+//Создать строку из символов этого массива.
